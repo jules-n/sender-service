fd4e0d6dd99c8dad06f363286e08e17e06710b50
ynero <ynero@softserveinc.com>
2021-09-09 14:01:35 +0300

    Init commit

---
+++ src/main/java/com/ynero/ss/senderservice/SenderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ynero.ss.senderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderServiceApplication.class, args);
+	}
+
+}
