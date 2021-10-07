79571e9e88fb42e3936481ec8b9f6dbba3543cda
ynero <ynero@softserveinc.com>
2021-10-07 15:20:21 +0300

    feature/SS-33: Fixed consuming type in kafka consumers

--- src/main/java/com/ynero/ss/senderservice/consumers/DeviceSendingDataConsumer.java
+++ src/main/java/com/ynero/ss/senderservice/consumers/DeviceSendingDataConsumer.java
@@ -3,6 +3,7 @@ package com.ynero.ss.senderservice.consumers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ynero.ss.senderservice.domain.DeviceSendingData;
 import com.ynero.ss.senderservice.persistence.DeviceSendingDataRepository;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -23,9 +24,11 @@ public class DeviceSendingDataConsumer {
         this.repository = repository;
     }
 
+    @SneakyThrows
     @KafkaListener(topics = "device-sending-data", groupId = "device-sending-data_group_id")
-    private void consume(LinkedHashMap message){
-        var data = objectMapper.convertValue(message, DeviceSendingData.class);
+    private void consume(String message){
+        var map = objectMapper.readValue(message, LinkedHashMap.class);
+        var data = objectMapper.convertValue(map, DeviceSendingData.class);
         data.setDataId(UUID.randomUUID());
         log.info(data);
         repository.save(data);
--- src/main/java/com/ynero/ss/senderservice/consumers/PipelineResultsConsumer.java
+++ src/main/java/com/ynero/ss/senderservice/consumers/PipelineResultsConsumer.java
@@ -3,6 +3,7 @@ package com.ynero.ss.senderservice.consumers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ynero.ss.senderservice.services.SendingDataSeeker;
 import dtos.ResultDTO;
+import lombok.SneakyThrows;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -20,9 +21,11 @@ public class PipelineResultsConsumer {
         this.sendingDataSeeker = sendingDataSeeker;
     }
 
+    @SneakyThrows
     @KafkaListener(topics = "results-sending-data", groupId = "results-sending-data_group_id")
-    private void consume(LinkedHashMap message){
-        List<List< ResultDTO >> dataList = objectMapper.convertValue(message, List.class);
+    private void consume(String message){
+        var map = objectMapper.readValue(message, LinkedHashMap.class);
+        List<List< ResultDTO >> dataList = objectMapper.convertValue(map, List.class);
         dataList.forEach(
                 innerList -> innerList.forEach(
                         dto -> sendingDataSeeker.find(dto)
