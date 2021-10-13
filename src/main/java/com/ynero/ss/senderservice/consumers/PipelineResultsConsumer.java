18e9b4da468f5a143597098752fe44b5e20ef5d4
ynero <ynero@softserveinc.com>
2021-10-13 03:52:45 +0300

    feature/SS-33: Added log in PipelineResultsConsumer

--- src/main/java/com/ynero/ss/senderservice/consumers/PipelineResultsConsumer.java
+++ src/main/java/com/ynero/ss/senderservice/consumers/PipelineResultsConsumer.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ynero.ss.senderservice.services.SendingDataSeeker;
 import dtos.ResultDTO;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import java.util.LinkedHashMap;
 import java.util.List;
 
 @Service
+@Log4j2
 public class PipelineResultsConsumer {
     private final ObjectMapper objectMapper;
 
@@ -24,6 +26,7 @@ public class PipelineResultsConsumer {
     @SneakyThrows
     @KafkaListener(topics = "results-sending-data", groupId = "results-sending-data_group_id")
     private void consume(String message){
+        log.info("message: {}", message);
         var map = objectMapper.readValue(message, List.class);
         List<List< ResultDTO >> dataList = objectMapper.convertValue(map, List.class);
         dataList.forEach(
